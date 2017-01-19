03d82ff448082a8219ea1679c7f3b2c7f6199dc7
chavesandres1009 <o.chaves@paralleldevs.com>
2017-01-19 11:23:11 -0600

    ticket MSNGR-7

--- sites/all/modules/custom/auto_fill_id/templates/field--field-item-compromiso--compromiso-de-gestion.tpl.php
+++ sites/all/modules/custom/auto_fill_id/templates/field--field-item-compromiso--compromiso-de-gestion.tpl.php
@@ -78,40 +78,61 @@ drupal_add_css(drupal_get_path('module', 'auto_fill_id') . '/css/compromisos_tab
             $accion_estrategica = $element['field_accion_collect'][0]['entity']['field_collection_item'][$accion_id];
 
             $acciones_cant = sizeof($element['field_accion_collect']['#items']);
-            /*$meta_cant = 0;
+            $meta_cant = 0;
             $productos_cant = 0;
             foreach($element['field_accion_collect']['#items'] as $i => $action_id) {
-              $meta_cant += sizeof($element['field_accion_collect'][$i]['entity']['field_collection_item'][$action_id['value']]['field_meta']['#items']);
-              //foreach($element['field_accion_collect'][$i]['entity']['field_collection_item'][$action_id['value']]['field_meta']['#items'] as $j => $meta_id) {
-                  foreach ($element['field_accion_collect'][$i]['entity']['field_collection_item'][$action_id['value']]['field_meta'][0]['node'] as $meta_field){
-                      $productos_cant += sizeof($meta_field['field_producto']['#items']);
-                  }
-              //}
-            }*/
-
+              $meta_cant += isset($element['field_accion_collect'][$i]['entity']['field_collection_item'][$action_id['value']]['field_meta']) ?
+                   sizeof($element['field_accion_collect'][$i]['entity']['field_collection_item'][$action_id['value']]['field_meta']['#items']) : 1;
+            }
+            $item_rowspan = $acciones_cant > $meta_cant ? $acciones_cant : $meta_cant;
+            $odd_even_class = $delta % 2 ? 'odd' : 'even';
           ?>
-          <!--<div class="field-item <?php print $delta % 2 ? 'odd' : 'even'; ?>"<?php print $item_attributes[$delta]; ?>>-->
-          <td rowspan="<?php print $acciones_cant ?>" class="taxonomy-td-item-<?php print $delta ?>"><?php print render($element['field_codigo_eje']); ?></td>
-          <td rowspan="<?php print $acciones_cant ?>" class="taxonomy-td-item-<?php print $delta ?>"><?php print render($element['field_codigo_ambito']); ?></td>
-          <td rowspan="<?php print $acciones_cant ?>" class="taxonomy-td-item-<?php print $delta ?>"><?php print render($element['field_codigo_lineamiento']); ?></td>
+          <!--<div class="field-item <?php //print $delta % 2 ? 'odd' : 'even'; ?>"<?php //print $item_attributes[$delta]; ?>>-->
+          <!--Taxonomias-->
+          <td rowspan="<?php print $item_rowspan ?>" class="taxonomy-td-item-<?php print $delta ?> <?php print $odd_even_class; ?>">
+            <?php print render($element['field_codigo_eje']); ?>
+          </td>
+          <td rowspan="<?php print $item_rowspan ?>" class="taxonomy-td-item-<?php print $delta ?> <?php print $odd_even_class; ?>">
+            <?php print render($element['field_codigo_ambito']); ?>
+          </td>
+          <td rowspan="<?php print $item_rowspan ?>" class="taxonomy-td-item-<?php print $delta ?> <?php print $odd_even_class; ?>">
+            <?php print render($element['field_codigo_lineamiento']); ?>
+          </td>
 
           <?php foreach($element['field_accion_collect']['#items'] as $i => $action_id) : ?>
             <?php if ($i > 0) :  ?><tr> <?php endif; ?>
-              <td>
+              <?php $action_rowspan = isset($element['field_accion_collect'][$i]['entity']['field_collection_item'][$action_id['value']]['field_meta']['#items']) ?
+                                    sizeof($element['field_accion_collect'][$i]['entity']['field_collection_item'][$action_id['value']]['field_meta']['#items']) : 1?>
+              <td rowspan="<?php print $action_rowspan ?>" class="<?php print $odd_even_class; ?>">
                 <?php
                     print render($element['field_accion_collect'][$i]['entity']['field_collection_item'][$action_id['value']]['field_codigo_accion_estrategica']);
                 ?>
               </td>
               <!--Meta-->
                 <?php if(!isset($element['field_accion_collect'][$i]['entity']['field_collection_item'][$action_id['value']]['field_meta'])) : ?>
-                    <td></td>
-                    <td></td>
+                    <td class="<?php print $odd_even_class; ?>"></td>
+                    <td class="<?php print $odd_even_class; ?>"></td>
             <?php else : ?>
-              <?php foreach ($element['field_accion_collect'][$i]['entity']['field_collection_item'][$action_id['value']]['field_meta'][0]['node'] as $meta_field) : ?>
-                <td><?php print $meta_field['#node']->title; ?></td>
-
-            <!--Producto-->
-                <td><?php print render($meta_field['field_producto']); ?> </td>
+              <?php foreach ($element['field_accion_collect'][$i]['entity']['field_collection_item'][$action_id['value']]['field_meta']['#items'] as $meta_id => $meta_field) : ?>
+                <?php if($meta_id > 0): ?> <tr> <?php endif; ?>
+                    <td class="<?php print $odd_even_class; ?>">
+                      <?php print render ($element['field_accion_collect'][$i]['entity']['field_collection_item'][$action_id['value']]['field_meta'][$meta_id]); ?>
+                    </td>
+                    <!--Producto-->
+                    <td class="<?php print $odd_even_class; ?>">
+                        <?php
+                            //dpm($meta_field);
+                            if(!empty($meta_field['entity']->field_producto)) {
+                                foreach ($meta_field['entity']->field_producto[LANGUAGE_NONE] as $producto_id) {
+                                    $product = node_load($producto_id['target_id']);
+                                    //dpm($product);
+                                    print $product->title . '</br>';
+                                }
+                            }
+                        ?>
+                    </td>
+                    <!--<td><?php //print render($meta_field['entity']->field_producto); ?></td>-->
+                <?php if($meta_id > 0): ?> </tr> <?php endif; ?>
               <?php endforeach; ?>
             <?php endif; ?>
             <?php if ($i > 0) :  ?></tr> <?php endif; ?>
