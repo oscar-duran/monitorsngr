322ecafef61b7b2e586fba706f1ce932b2fee75f
chavesandres1009 <o.chaves@paralleldevs.com>
2017-04-07 14:27:29 -0600

    Changes on settings.php

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -667,10 +667,8 @@ $conf['master_modules']['base'] = array(
 
   // Custom modules.
   'autoevaluacion',
-  'auto_fill_id',
   'cne_alters',
   'compromiso_gestion',
-  'feature_sngr_vistas_de_compromisos_de_gesti_n',
   'field_permissions',
   'field_readonly',
   'lista_modulos',
